9eefd0f1a1103072a0834ca3cacc06b850ca1875
KidusMT <kidusmamuye@gmail.com>
2021-03-31 16:54:19 -0500

    updated the login branch with the changes made by @anteneh from develop branch

---
+++ src/controllers/LoginController.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import common.utils.UserSession;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import main.Main;
+import views.admin.Admin;
+import views.librarian.Librarian;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    Label errorLabel;
+
+    private Admin admin;
+    private Librarian librarian;
+
+    public LoginController() {
+        admin = new Admin();
+        librarian = new Librarian();
+    }
+
+    @FXML
+    public void loginButtonController(ActionEvent event) throws Exception {
+        Stage stage = new Stage();
+        Main.primaryStage.hide();
+        if (usernameField.getText().equals("admin") && passwordField.getText().equals("admin")) {
+            UserSession.createInstance("admin@gmail.com", UserSession.Role.Admin);
+            admin.start(stage);
+        } else if (usernameField.getText().equals("member") && passwordField.getText().equals("member")) {
+            UserSession.createInstance("member@gmail.com", UserSession.Role.User);
+            librarian.start(stage);
+        } else
+            errorLabel.setText("Incorrect Id/Password");
+
+    }
+}
--- src/views/login/Login.java
+++ src/views/login/Login.java
@@ -1,43 +1,12 @@
 package views.login;
 
-import common.utils.UserSession;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.application.Application;
 import javafx.stage.Stage;
-import main.Main;
-import views.admin.Admin;
-import views.librarian.Librarian;
 
-public class Login {
-    @FXML
-    TextField id;
+public class Login extends Application {
 
-    @FXML
-    PasswordField passwordField;
-    @FXML
-    Label errorLabel;
+    @Override
+    public void start(Stage stage) throws Exception {
 
-    private Admin admin;
-    private Librarian librarian;
-
-    public Login() {
-        admin = new Admin();
-        librarian = new Librarian();
-    }
-
-    public void loginHandler(ActionEvent event) throws Exception {
-        Stage stage = new Stage();
-        Main.primaryStage.hide();
-        if (id.getText().equals("admin") && passwordField.getText().equals("admin")) {
-            UserSession.createInstance("admin@gmail.com", UserSession.Role.Admin);
-            admin.start(stage);
-        } else if (id.getText().equals("member") && passwordField.getText().equals("member")) {
-            UserSession.createInstance("member@gmail.com", UserSession.Role.User);
-            librarian.start(stage);
-        } else
-            errorLabel.setText("Incorrect Id/Password");
     }
 }
