c684a93c3966a33a801bcbe7dda02587f5f3f0d2
Abenezer Sintayehu <abensinte123@gmail.com>
2021-03-31 20:44:18 -0500

    added view-books ui

--- src/views/admin/viewBooks/ViewBooks.java
+++ src/views/admin/viewBooks/ViewBooks.java
@@ -1,4 +1,15 @@
 package views.admin.viewBooks;
 
+import javafx.event.ActionEvent;
+import views.admin.Admin;
+
+import java.io.IOException;
+
 public class ViewBooks {
+    public void navigateToMembersHandler(ActionEvent event) throws IOException {
+        Admin.routeToViewMembers();
+    }
+    public void navigateToCreateBook(ActionEvent event) throws IOException{
+        Admin.routeToCreateBook();
+    }
 }
