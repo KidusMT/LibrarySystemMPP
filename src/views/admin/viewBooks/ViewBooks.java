3827b65d8cdf9f9c1e3da8c496f30d9e4c21e5f2
Abenezer Sintayehu <abensinte123@gmail.com>
2021-04-01 10:53:51 -0500

    minor-update

--- src/views/admin/viewBooks/ViewBooks.java
+++ src/views/admin/viewBooks/ViewBooks.java
@@ -1,8 +1,6 @@
 package views.admin.viewBooks;
 
-import common.utils.UserSession;
 import javafx.event.ActionEvent;
-import main.Main;
 import views.admin.Admin;
 
 import java.io.IOException;
@@ -11,14 +9,7 @@ public class ViewBooks {
     public void navigateToMembersHandler(ActionEvent event) throws IOException {
         Admin.routeToViewMembers();
     }
-
-    public void navigateToCreateBook(ActionEvent event) throws IOException {
+    public void navigateToCreateBook(ActionEvent event) throws IOException{
         Admin.routeToCreateBook();
-    }
-
-    public void navigateToLogin(ActionEvent event) throws IOException {
-        UserSession.destroySession();
-        Admin.stage.hide();
-        Main.primaryStage.show();
     }
 }
