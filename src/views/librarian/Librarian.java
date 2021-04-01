e2ca39d581372eb3ae10532a821e512787c1d835
KidusMT <kidusmamuye@gmail.com>
2021-04-01 15:38:53 -0500

    updated on the ViewCheckout controller class location logics

--- src/views/librarian/Librarian.java
+++ src/views/librarian/Librarian.java
@@ -21,7 +21,7 @@ public class Librarian extends Application {
     }
 
     public static void routeToViewCheckouts() throws IOException {
-        route(FXMLLoader.load(Librarian.class.getResource("/views/librarian/viewCheckouts/view-checkout.fxml")), "View checkout records");
+        route(FXMLLoader.load(Librarian.class.getResource("/views/librarian/viewCheckout/view-checkout.fxml")), "View checkout records");
     }
 
     private static void route(Parent contentRoot, String title) {
@@ -32,11 +32,11 @@ public class Librarian extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("/views/librarian/viewCheckouts/view-checkout.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/librarian/viewCheckout/view-checkout.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("MIU Library System");
         stage.getIcons().add(new Image(getClass().getResource("/views/login/miu_logo_app.png").toURI().toString()));
-        scene.getStylesheets().add(getClass().getResource("/views/librarian/viewCheckouts/view_checkout.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/views/librarian/viewCheckout/view_checkout.css").toExternalForm());
         scene.getStylesheets().add(getClass().getResource("/common/styles/common.css").toExternalForm());
         stage.setScene(scene);
         stage.setFullScreenExitHint("");
