dda915d94e2356fe9a106237d7ba85c18429a3d3
KidusMT <kidusmamuye@gmail.com>
2021-04-01 17:50:29 -0500

    updated the UI interface for the librarian features

--- src/views/librarian/updateCheckout/UpdateCheckout.java
+++ src/views/librarian/updateCheckout/UpdateCheckout.java
@@ -1,11 +1,15 @@
 package views.librarian.updateCheckout;
 
 import javafx.event.ActionEvent;
+import views.librarian.Librarian;
+
+import java.io.IOException;
 
 public class UpdateCheckout {
     public void createCheckout(ActionEvent event) {
     }
 
-    public void navigateToViewCheckoutRecords(ActionEvent event) {
+    public void navigateToViewCheckoutRecords(ActionEvent event) throws IOException {
+        Librarian.routeToViewCheckouts();
     }
 }
--- src/views/librarian/updateCheckoutEntry/UpdateCheckoutEntry.java
+++ src/views/librarian/updateCheckoutEntry/UpdateCheckoutEntry.java
@@ -1,4 +1,13 @@
 package views.librarian.updateCheckoutEntry;
 
+import javafx.event.ActionEvent;
+import views.librarian.Librarian;
+
+import java.io.IOException;
+
 public class UpdateCheckoutEntry {
+
+    public void navigateToViewCheckoutRecords(ActionEvent event) throws IOException {
+        Librarian.routeToViewCheckouts();
+    }
 }
