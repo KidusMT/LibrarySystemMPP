4bd1f76e2ea22dde2d1c0252de5446b0e2bbd481
Anteneh <anteneh845@gmail.com>
2021-04-02 00:22:39 -0500

    updated search for view books and view members

--- src/views/admin/viewBooks/ViewBooks.java
+++ src/views/admin/viewBooks/ViewBooks.java
@@ -1,7 +1,7 @@
 package views.admin.viewBooks;
 
-import common.utils.DataAccess;
 import common.utils.UserSession;
+import controllers.BookController;
 import controllers.SystemController;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
@@ -13,48 +13,68 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import main.Main;
-import models.*;
+import models.Author;
+import models.Book;
+import models.BookCopy;
 import views.admin.Admin;
 
-import javax.print.attribute.standard.Copies;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class ViewBooks implements Initializable {
-    @FXML private TableView<Book> tableView;
-    @FXML private TableColumn<Book, String> isbnCol;
-    @FXML private TableColumn<Book,String> titleCol;
-    @FXML private TableColumn<Book,Author> authorCol;
-    @FXML private TableColumn<Book,Integer> copiesCol;
-    @FXML private TableColumn<Book,Integer> availableCol;
-    @FXML private TextField searchText;
+    @FXML
+    private TableView<Book> tableView;
+    @FXML
+    private TableColumn<Book, String> isbnCol;
+    @FXML
+    private TableColumn<Book, String> titleCol;
+    @FXML
+    private TableColumn<Book, Author> authorCol;
+    @FXML
+    private TableColumn<Book, Integer> copiesCol;
+    @FXML
+    private TableColumn<Book, Integer> availableCol;
+    @FXML
+    private TextField searchText;
+    private BookController bookController;
+
+    public ViewBooks() {
+        bookController = new BookController();
+    }
 
     public void navigateToMembersHandler(ActionEvent event) throws IOException {
         Admin.routeToViewMembers();
     }
+
     public void navigateToBooksHandler(ActionEvent event) throws IOException {
         Admin.routeViewBooks();
     }
-    public void navigateToCreateBook(ActionEvent event) throws IOException{
+
+    public void navigateToCreateBook(ActionEvent event) throws IOException {
         Admin.routeToCreateBook();
     }
+
     public void navigateToLogin(ActionEvent event) throws IOException {
         UserSession.destroySession();
         Admin.stage.hide();
         Main.primaryStage.show();
     }
-    public void searchBookHandler(ActionEvent event){
-        String searchString = searchText.getText();
 
+    public void searchBookHandler(ActionEvent event) {
+        String searchString = searchText.getText();
         List<Book> books = new ArrayList<>();
-        for(Book book : allBooks()){
-            if(book.getIsbn().equals(searchString) || book.getTitle().equals(searchString)) books.add(book);
+        if (searchString.trim().equals(""))
+            populateTable(bookController.getAllBooks());
+        else {
+            for (Book book : allBooks()) {
+                if (book.getIsbn().equals(searchString) || book.getTitle().contains(searchString))
+                    books.add(book);
+            }
+            populateTable(books);
         }
-        populateTable(books);
     }
 
     @Override
@@ -63,36 +83,36 @@ public class ViewBooks implements Initializable {
         List<Book> bookList = systemController.getAllBooks();
         populateTable(bookList);
     }
-    public void populateTable(List<Book> bookList){
-        isbnCol.setCellValueFactory(new PropertyValueFactory<Book,String >("isbn"));
-        titleCol.setCellValueFactory(new PropertyValueFactory<Book,String>("title"));
-        authorCol.setCellValueFactory(new PropertyValueFactory<Book,Author>("authors"));
+
+    public void populateTable(List<Book> bookList) {
+        isbnCol.setCellValueFactory(new PropertyValueFactory<Book, String>("isbn"));
+        titleCol.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+        authorCol.setCellValueFactory(new PropertyValueFactory<Book, Author>("authors"));
         copiesCol.setCellValueFactory(entry -> new SimpleObjectProperty<Integer>(entry.getValue().getCopies().length));
         availableCol.setCellValueFactory(entry -> {
             int count = 0;
-            for(BookCopy bookCopy: entry.getValue().getCopies()){
-                if (bookCopy.isAvailable()) count ++;
+            for (BookCopy bookCopy : entry.getValue().getCopies()) {
+                if (bookCopy.isAvailable()) count++;
             }
             new SimpleObjectProperty<Boolean>(entry.getValue().isAvailable());
             return new SimpleObjectProperty<>(count);
         });
         tableView.getItems().setAll(bookList);
-        tableView.setRowFactory(tv->{
+        tableView.setRowFactory(tv -> {
             TableRow<Book> tableRow = new TableRow<>();
-            tableRow.setOnMouseClicked(event ->{
+            tableRow.setOnMouseClicked(event -> {
                 Book rowData = tableRow.getItem();
                 try {
                     Admin.routeToBookDetail(rowData);
                 } catch (IOException e) {
-                    e.printStackTrace();
                 }
             });
             return tableRow;
         });
     }
-    private List<Book> allBooks(){
-        List<Book> books = Arrays.asList(new Book("123","War and Peace",21,Arrays.asList(new Author("Leo","Tolstoy",null,null,null))),
-                                        new Book("234","Crime and Punishment",21,Arrays.asList(new Author("Leo","Tolstoy",null,null,null))));
+
+    private List<Book> allBooks() {
+        List<Book> books = bookController.getAllBooks();
         return books;
     }
 }
--- src/views/admin/viewMembers/ViewMembers.java
+++ src/views/admin/viewMembers/ViewMembers.java
@@ -1,39 +1,46 @@
 package views.admin.viewMembers;
 
 import common.utils.UserSession;
-import controllers.ControllerInterface;
 import controllers.MemberController;
-import controllers.SystemController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import main.Main;
-import models.Address;
-import models.Author;
-import models.Book;
 import models.LibraryMember;
 import views.admin.Admin;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class ViewMembers implements Initializable {
-    @FXML private TableView tableView;
-    @FXML private TableColumn idCol;
-    @FXML private TableColumn fNameCol;
-    @FXML private TableColumn lNameCol;
-    @FXML private TableColumn telephoneCol;
-    @FXML private TableColumn addressCol;
-    @FXML private TextField searchText;
+    @FXML
+    private TableView tableView;
+    @FXML
+    private TableColumn idCol;
+    @FXML
+    private TableColumn fNameCol;
+    @FXML
+    private TableColumn lNameCol;
+    @FXML
+    private TableColumn telephoneCol;
+    @FXML
+    private TableColumn addressCol;
+    @FXML
+    private TextField searchText;
 
     private MemberController memberController = new MemberController();
 
     public void navigateToBooksHandler() throws IOException {
         Admin.routeViewBooks();
     }
+
     public void navigateToMembersHandler(ActionEvent event) {
         List<LibraryMember> memberList = memberController.getAllMembers();
         System.out.println(memberList.isEmpty());
@@ -48,31 +55,36 @@ public class ViewMembers implements Initializable {
         Admin.stage.hide();
         Main.primaryStage.show();
     }
-    public void searchMemberHandler(ActionEvent event){
-        String searchString = searchText.getText();
 
+    public void searchMemberHandler(ActionEvent event) {
+        String searchString = searchText.getText();
         List<LibraryMember> members = new ArrayList<>();
-        for(LibraryMember member : allMembers()){
-            if(member.getFirstName().equals(searchString) || member.getLastName().equals(searchString) || member.getMemberId().equals(searchString)) members.add(member);
+        if (searchString.trim().equals(""))
+            populateTable(memberController.getAllMembers());
+        else {
+            for (LibraryMember member : allMembers())
+                if (member.getFirstName().contains(searchString) || member.getLastName().contains(searchString) || member.getMemberId().equals(searchString))
+                    members.add(member);
+
+            populateTable(members);
         }
-        populateTable(members);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    	MemberController m = new MemberController();
-        populateTable(m.getAllMembers());
+        populateTable(memberController.getAllMembers());
     }
-    public void populateTable(List<LibraryMember> memberList){
-        idCol.setCellValueFactory(new PropertyValueFactory<LibraryMember,String >("memberId"));
-        fNameCol.setCellValueFactory(new PropertyValueFactory<LibraryMember,String>("firstName"));
-        lNameCol.setCellValueFactory(new PropertyValueFactory<LibraryMember,String>("lastName"));
-        telephoneCol.setCellValueFactory(new PropertyValueFactory<LibraryMember,String>("telephone"));
-        addressCol.setCellValueFactory(new PropertyValueFactory<LibraryMember,String>("address"));
+
+    public void populateTable(List<LibraryMember> memberList) {
+        idCol.setCellValueFactory(new PropertyValueFactory<LibraryMember, String>("memberId"));
+        fNameCol.setCellValueFactory(new PropertyValueFactory<LibraryMember, String>("firstName"));
+        lNameCol.setCellValueFactory(new PropertyValueFactory<LibraryMember, String>("lastName"));
+        telephoneCol.setCellValueFactory(new PropertyValueFactory<LibraryMember, String>("telephone"));
+        addressCol.setCellValueFactory(new PropertyValueFactory<LibraryMember, String>("address"));
         tableView.getItems().setAll(memberList);
     }
-    private List<LibraryMember> allMembers(){
-        MemberController memberController = new MemberController();
+
+    private List<LibraryMember> allMembers() {
         return memberController.getAllMembers();
     }
 }
