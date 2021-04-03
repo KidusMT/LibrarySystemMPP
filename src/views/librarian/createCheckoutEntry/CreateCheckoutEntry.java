4f6d38f0e955620a57d216a3b6ad55d3e57a4877
KidusMT <kidusmamuye@gmail.com>
2021-04-02 20:43:17 -0500

    entity creation with due-date working properly

--- src/views/librarian/createCheckoutEntry/CreateCheckoutEntry.java
+++ src/views/librarian/createCheckoutEntry/CreateCheckoutEntry.java
@@ -10,6 +10,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import models.Book;
+import models.BookCopy;
 import models.LibraryMember;
 import views.View;
 
@@ -22,6 +23,14 @@ import java.util.Random;
 
 public class CreateCheckoutEntry {
 
+    private static LibraryMember libraryMember;
+    String selectedBook = "";
+    Book selectedBookCombo = null;
+    BookCopy selectedBookCopyCombo = null;
+    CheckoutEntityController entityController;
+    BookController bookController;
+    List<String> books = new ArrayList<>();
+    List<Book> bookListDb = new ArrayList<>();
     @FXML
     private TextField firstName;
     @FXML
@@ -30,18 +39,14 @@ public class CreateCheckoutEntry {
     private DatePicker dateBorrowed;
     @FXML
     private DatePicker dueDate;
-
     @FXML
     private ComboBox bookList;
-
     @FXML
     private TextField bookISBN;
     @FXML
     private Label errorMessage;
 
-    private static LibraryMember libraryMember;
-
-    public static void newInstance(LibraryMember record){
+    public static void newInstance(LibraryMember record) {
         libraryMember = record;
     }
 
@@ -50,10 +55,7 @@ public class CreateCheckoutEntry {
         LocalDate localDate = LocalDate.parse(dateString, formatter);
         return localDate;
     }
-    CheckoutEntityController entityController;
-    BookController bookController;
-    List<String> books = new ArrayList<>();
-    List<Book> bookListDb = new ArrayList<>();
+
     @FXML
     public void initialize() {
         entityController = new CheckoutEntityController();
@@ -81,36 +83,54 @@ public class CreateCheckoutEntry {
 //        CheckoutEntityController entityController = new CheckoutEntityController();
 //        BookController bookController = new BookController();
 
-        String selectedBook = bookList.getSelectionModel().getSelectedItem().toString();
+//        selectedBook = bookList.getSelectionModel().getSelectedItem().toString();
+//        System.out.println(selectedBook);
+//
+//        for (int i = 0; i < bookListDb.size(); i++) {
+//            if (selectedBook.equals(bookListDb.get(i).getTitle())) {
+//                selectedBookCombo = bookListDb.get(i);
+//                break;
+//            }
+//        }
+        if (selectedBookCombo != null) {
+            for (int i = 0; i < selectedBookCombo.getCopies().length; i++) {
+                if (selectedBookCombo.getCopies()[i].isAvailable()) {
+                    selectedBookCopyCombo = selectedBookCombo.getCopies()[i];
+                    int recordId = new Random().nextInt(1000 - 1) + 1;
+                    entityController.newCheckoutEntity(String.valueOf(recordId), libraryMember.getMemberId(),
+                            dateBorrowed.getValue(), dueDate.getValue(), selectedBookCopyCombo);
+
+                    View.routeToViewCheckouts();
+                    break;
+                } else {
+                    // tell the user there is no book available
+                    errorMessage.setText("No book copy found for this book.");
+                }
+            }
+        }
+    }
+
+    public void comboAction(ActionEvent event) {
+        selectedBook = bookList.getSelectionModel().getSelectedItem().toString();
         System.out.println(selectedBook);
-        Book selectedBookCombo = null;
+
         for (int i = 0; i < bookListDb.size(); i++) {
             if (selectedBook.equals(bookListDb.get(i).getTitle())) {
                 selectedBookCombo = bookListDb.get(i);
                 break;
             }
         }
-        if (selectedBookCombo != null) {
-            for (int i = 0; i < selectedBookCombo.getCopies().length; i++) {
-                if(selectedBookCombo.getCopies()[i].isAvailable()){
-                    int recordId = new Random().nextInt(1000 - 1) + 1;
-                    entityController.newCheckoutEntity(String.valueOf(recordId), libraryMember.getMemberId(),
-                            dateBorrowed.getValue(), dueDate.getValue(), selectedBookCombo.getCopies()[i]);
 
-                    View.routeToViewCheckouts();
-                    break;
-                }else{
-                    // tell the user there is no book available
-                    errorMessage.setText("No book copy found for this book.");
-                }
-
-            }
+        if(selectedBookCombo!=null){
+            int maxDateLength = selectedBookCombo.getMaxCheckoutLength();
+            LocalDate due= dateBorrowed.getValue().plusDays(maxDateLength);
+            dueDate.setValue(due);
         }
     }
 
     public void findBook(ActionEvent event) {
         String isbn = bookISBN.getText().trim();
         System.out.println(isbn);
-        errorMessage.setText(isbn+" couldn't be found.");
+        errorMessage.setText(isbn + " couldn't be found.");
     }
 }
